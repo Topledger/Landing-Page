4970f3b173af8545159021c085ef8116dc8f0468
Dishant Agnihotri <dishantagnihotri@gmail.com>
2022-09-08 21:28:19 +0530

    Reverted to previous codebase

--- README.md
+++
@@ -1,4 +0,0 @@
-## Top Ledger Website v2
-
-Designed by Lokesh Tiwari
-Developed by [Dishant Agnihotri](https://dishantagnihotri.com)
--- context/DarkMode/index.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-
-export const DarkModeContext = createContext({isDarkMode:null, setIsDarkMode: () => null});
