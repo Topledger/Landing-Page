6e5b209b675f74a92854f7c6cf94667c1eab0021
dkdocs <deepak@topledger.xyz>
2022-09-19 19:01:47 +0530

    updates copy

--- components/ChainSection/index.js
+++ components/ChainSection/index.js
@@ -8,7 +8,7 @@ const ChainSection = () => {
           <div className="offerContent offerContent2">
             <h1>Collate off-chain and on-chain data</h1>
             <p>
-            Connect your private data source securely into the Top Ledger platform and combine it with on-chain Solana smart contract data.</p>
+            Connect your private data source securely into the Top Ledger platform and combine it with on-chain Solana program data.</p>
             <p>  Answer complex questions, strengthen decision making and finally share the enriched dashboards with your key stakeholders.  </p>
           </div>
           <div className="offerImage hasPad">
