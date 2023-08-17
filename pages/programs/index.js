c4c8f5cd27e55f60c08016634436d0f062462e62
manishsharma004 <manishsharma004@yahoo.com>
2023-08-17 15:25:29 +0530

    - Fix: Page title "User Engagement Tracker | Top Ledger" - Add SEO params for tracker dashboard page

--- pages/programs/index.js
+++ pages/programs/index.js
@@ -8,6 +8,18 @@ import { fetchProgramList } from "queries";
 import styles from "./programs.module.scss";
 import ProgramAdressInput from "./components/ProgramAddressInput";
 import Loader from "./components/Loader";
+import Head from "next/head";
+
+const DashboardHead = ({ programName }) => (
+  <Head>
+    <title>User Engagement Tracker | Top Ledger</title>
+    <meta name="description" content=""></meta>
+    <meta
+      name="keywords"
+      content={`${programName}, User Engagement Tracker, Top Ledger, Crypto Analytics, Solana Analytics, Solana Blockchain`}
+    ></meta>
+  </Head>
+);
 
 const TLDashboards = dynamic(
   async () => {
@@ -56,8 +68,6 @@ function Programs() {
     [address]
   );
 
-  address;
-
   const updateParameterAddress = useCallback((address) => {
     const parameters = parametersRef.current;
     if (parameters) {
@@ -82,51 +92,54 @@ function Programs() {
   }, [address]);
 
   return (
-    <div className={styles.programContainer}>
-      <div className="dashboard">
-        <ProgramAdressInput isDashboard onApply={() => {}} />
-        <>
-          <div className="dashboard-component">
-            <div className="title">
-              <span className="title-text">{title}</span>
-              <span className="title-subtext">{subTitle}</span>
-            </div>
-            {isLoading && <Loader />}
-            <div
-              className={cx(styles.dashboardContainer, { dashboardLoading })}
-            >
-              <TLDashboards
-                key={address}
-                client="tl"
-                token="oIEupNW8g4Ua9C64JvUsYRLNlOZej940x341KaAH"
-                className={styles.dashboard}
-                loader={<Loader />}
-                onDashboardLoad={handleDashboardLoad}
-                parameters={parametersRef.current}
-                dashboardRef={dashboardRef}
-              />
-            </div>
-            {!isLoading && !dashboardLoading && (
-              <div className={styles.talkToUs}>
-                <a
-                  title="telegram"
-                  draggable="false"
-                  href="https://telegram.me/ergon50"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    draggable="false"
-                    src="/assets/images/telegram-icon.svg"
-                  />
-                  Talk to us
-                </a>
+    <>
+      <DashboardHead programName={title} />
+      <div className={styles.programContainer}>
+        <div className="dashboard">
+          <ProgramAdressInput isDashboard onApply={() => {}} />
+          <>
+            <div className="dashboard-component">
+              <div className="title">
+                <span className="title-text">{title}</span>
+                <span className="title-subtext">{subTitle}</span>
               </div>
-            )}
-          </div>
-        </>
+              {isLoading && <Loader />}
+              <div
+                className={cx(styles.dashboardContainer, { dashboardLoading })}
+              >
+                <TLDashboards
+                  key={address}
+                  client="tl"
+                  token="oIEupNW8g4Ua9C64JvUsYRLNlOZej940x341KaAH"
+                  className={styles.dashboard}
+                  loader={<Loader />}
+                  onDashboardLoad={handleDashboardLoad}
+                  parameters={parametersRef.current}
+                  dashboardRef={dashboardRef}
+                />
+              </div>
+              {!isLoading && !dashboardLoading && (
+                <div className={styles.talkToUs}>
+                  <a
+                    title="telegram"
+                    draggable="false"
+                    href="https://telegram.me/ergon50"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      draggable="false"
+                      src="/assets/images/telegram-icon.svg"
+                    />
+                    Talk to us
+                  </a>
+                </div>
+              )}
+            </div>
+          </>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
