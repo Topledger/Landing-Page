d5bd222d57f0017e7a7bc35ff4d18d53209a4283
urluckyturtle01 <101178012+urluckyturtle01@users.noreply.github.com>
2023-10-17 22:53:55 +0530

    Update index.js

--- components/ChromeExtensionSection/index.js
+++ components/ChromeExtensionSection/index.js
@@ -28,9 +28,8 @@ export const ChromeExtensionSection = () => {
     <section className={styles.section}>
       <div className={styles.textSection}>
         <h3>Chrome Extension</h3>
-        <p>Enhance audience engagement with our Chrome extension. Seamlessly integrate your public dashboard from your Top Ledger account to elevate community interactions. As visitors navigate your website, news articles about your project, your documentation page, etc., they'll have instant access to your dashboard. Plus, manage everything in real-time via Top Ledger.
-        </p>
-        <p style={{ textAlign: "left", margin-top: "1.5rem" }}>
+        <p>Enhance audience engagement with our Chrome extension. Seamlessly integrate your public dashboard from your Top Ledger account to elevate community interactions. As visitors navigate your website, news articles about your project, your documentation page, etc., they'll have instant access to your dashboard. Plus, manage everything in real-time via Top Ledger.</p>
+        <p style={{ textAlign: "left", marginTop: "1.5rem" }}>
           <AddToChromeButton />
         </p>
       </div>
