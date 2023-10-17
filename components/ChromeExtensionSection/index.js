ae8960396393333945dc0b514de17bc9bb2207b3
urluckyturtle01 <101178012+urluckyturtle01@users.noreply.github.com>
2023-10-17 22:33:29 +0530

    Update index.js

--- components/ChromeExtensionSection/index.js
+++ components/ChromeExtensionSection/index.js
@@ -29,7 +29,7 @@ export const ChromeExtensionSection = () => {
       <div className={styles.textSection}>
         <h3>Chrome Extension</h3>
         <p>
-          Explore the vital statistics of any Solana-based project at a glance! With the Top Ledger chrome extension, effortlessly access key metrics such as transactions, active wallets, volume, TVL, and even comparative analysis.<br />Seamlessly integrate in-depth insights into your browsing experience and unlock a wealth of blockchain analysis, all within your browser!
+  Enhance audience engagement with our Chrome extension. Seamlessly integrate your public dashboard from your Top Ledger account to elevate community interactions. As visitors navigate your website, news articles about your project, your documentation page, etc., they'll have instant access to your dashboard. Plus, manage everything in real-time via Top Ledger.
         </p>
         <p style={{ textAlign: "left", marginTop: "1.5rem" }}>
           <AddToChromeButton />
