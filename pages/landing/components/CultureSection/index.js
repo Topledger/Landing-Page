aa0e2d30cc566fecdfba620f78d137e1259149be
manishsharma004 <51284079+manishsharma004@users.noreply.github.com>
2024-02-23 06:47:16 +0000

    build fix

--- pages/landing/components/CultureSection/index.js
+++ pages/landing/components/CultureSection/index.js
@@ -7,6 +7,7 @@ import Image from "next/image";
 import Icon from "@/components/Icon";
 import Button from "@/components/Button";
 import UnderlinedText from "@/components/UnderlinedText";
+import { getId } from "helpers/utils";
 
 const Culture = ({
   image,
