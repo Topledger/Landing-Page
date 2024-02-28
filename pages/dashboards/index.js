4d66f0016e70ba4ffb1982d7e62600fee3a6dbc8
urluckyturtle01 <101178012+urluckyturtle01@users.noreply.github.com>
2024-02-28 15:34:11 +0530

    Update index.js

--- pages/dashboards/index.js
+++ pages/dashboards/index.js
@@ -14,6 +14,7 @@ export const DASHBOARD_ICONS = {
   Drip: "/assets/images/dashboards/drip logo.svg",
   "Drift trade": "/assets/images/dashboards/drift logo.svg",
   Helium: "/assets/images/dashboards/helium.svg",
+  "Helium mobile subscribers": "/assets/images/dashboards/helium.svg",
   Phoenix: "/assets/images/dashboards/phoenix.svg",
   Tensor: "/assets/images/dashboards/tensor logo.svg",
   "Flash trade": "/assets/images/dashboards/flash trade.svg",
