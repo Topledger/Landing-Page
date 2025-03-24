2d31f635d4173df3977929f418b4c3c8748ca348
manishsharma004 <manishsharma004@yahoo.com>
2025-03-24 21:32:09 +0530

    feat(state-of-solana): build fix

--- pages/state-of-solana/index.js
+++ pages/state-of-solana/index.js
@@ -1,16 +1,17 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import { userWalletDashboards } from "constants/constants";
 
 const StateOfSolanaRedirect = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        router.replace(
+            `/state-of-solana/${userWalletDashboards[0].wallet_dashboard.slug}`
+        );
+    }, [router]);
+
     return null;
 };
-
-export async function getServerSideProps() {
-    return {
-        redirect: {
-            destination: `/state-of-solana/${userWalletDashboards[0].wallet_dashboard.slug}`,
-            permanent: false,
-        },
-    };
-}
 
 export default StateOfSolanaRedirect;
