36869efe1f2b2d71322ece0ef6e62a4d52e0365c
manishsharma004 <manishsharma004@yahoo.com>
2025-03-25 22:57:11 +0530

    feat(state-of-solana): fix footer state-of-solana pages

--- pages/_app.js
+++ pages/_app.js
@@ -17,70 +17,78 @@ const footerExcludePaths = ["thanks", "dashboard", "programs"];
 const headerExcludePaths = ["thanks"];
 
 function isExcludedPath(pathList, currentPath) {
-  return pathList.findIndex((path) => currentPath.includes(path)) === -1;
+    return pathList.findIndex((path) => currentPath.includes(path)) === -1;
 }
 
 const NO_HEADER_FOOTER_PATHS = [
-  "/",
-  "/get-started",
-  "/landing",
-  "/web3-teams",
-  "/pricing",
-  "/dashboards",
-  "/research",
-  "/privacy-policy",
-  "/tnc",
-  "/state-of-solana",
+    /^\/$/,
+    /^\/get-started$/,
+    /^\/landing$/,
+    /^\/web3-teams$/,
+    /^\/pricing$/,
+    /^\/dashboards$/,
+    /^\/research$/,
+    /^\/privacy-policy$/,
+    /^\/tnc$/,
+    /^\/state-of-solana$/,
+    /^\/state-of-solana\/.*$/,
 ];
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  const queryClient = useMemo(() => new QueryClient(), []);
+    const router = useRouter();
+    const queryClient = useMemo(() => new QueryClient(), []);
 
-  useEffect(() => {
-    if (router.pathname.includes("about-us")) {
-      document.body.style.background = "white";
-    }
-  }, []);
+    useEffect(() => {
+        if (router.pathname.includes("about-us")) {
+            document.body.style.background = "white";
+        }
+    }, []);
 
-  const showFooter = isExcludedPath(footerExcludePaths, router.pathname);
-  const showHeader = isExcludedPath(headerExcludePaths, router.pathname);
+    useEffect(() => {
+        initGa({ trackingId: TRACKING_ID });
+    }, []);
 
-  useEffect(() => {
-    initGa({ trackingId: TRACKING_ID });
-  }, []);
+    const noHeaderAndFooter = useMemo(
+        () => NO_HEADER_FOOTER_PATHS.some((path) => path.test(router.pathname)),
+        [router.pathname]
+    );
 
-  return (
-    <>
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" type="image/x-icon" href="/favicon.svg" />
-      </Head>
-      <QueryClientProvider client={queryClient}>
-        <DarkModeProvider>
-          {(() => {
-            if (NO_HEADER_FOOTER_PATHS.includes(router.pathname)) {
-              return <Component {...pageProps} />;
-            }
+    return (
+        <>
+            <Head>
+                <meta charSet="UTF-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+                <link rel="icon" type="image/x-icon" href="/favicon.svg" />
+            </Head>
+            <QueryClientProvider client={queryClient}>
+                <DarkModeProvider>
+                    {(() => {
+                        if (noHeaderAndFooter) {
+                            return <Component {...pageProps} />;
+                        }
 
-            return (
-              <>
-                {router.pathname.includes("thanks") ? null : <Header />}
-                <Component {...pageProps} />
-                {router.pathname.includes("thanks") ||
-                  router.pathname.includes("dashboard") ? null : (
-                  <Footer />
-                )}
-              </>
-            );
-          })()}
-        </DarkModeProvider>
-        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-      </QueryClientProvider>
-    </>
-  );
+                        return (
+                            <>
+                                {router.pathname.includes("thanks") ? null : (
+                                    <Header />
+                                )}
+                                <Component {...pageProps} />
+                                {router.pathname.includes("thanks") ||
+                                router.pathname.includes("dashboard") ? null : (
+                                    <Footer />
+                                )}
+                            </>
+                        );
+                    })()}
+                </DarkModeProvider>
+                {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+            </QueryClientProvider>
+        </>
+    );
 }
 
 export default MyApp;
